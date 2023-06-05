a2975636d5f3b1c77846bc1762b5124e831751bb
Moritz <75447721+Markus-info@users.noreply.github.com>
2023-06-05 16:35:44 +0200

    COMMIT PHONE ERROR RESOLVE

--- app/src/main/java/com/noritz/whatsup/Verification.kt
+++ app/src/main/java/com/noritz/whatsup/Verification.kt
@@ -159,11 +159,11 @@ class Verification : AppCompatActivity() {
 
     inner class EditTextWatcher(private val view : View) : TextWatcher {
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            TODO("Not yet implemented")
+            //TODO("Not yet implemented")
         }
 
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            TODO("Not yet implemented")
+            //TODO("Not yet implemented")
         }
 
         override fun afterTextChanged(s: Editable?) {
